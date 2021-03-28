890e0a360706ed658a37fe7c5d1e8db0c74811d3
vixo341 <malpa341@gmail.com>
2021-03-28 11:49:45 +0200

    xamarin app

---
+++ MusicStreamingApplication/MusicStreamingApplication/MusicStreamingApplication/Models/Songs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicStreamingApplication.Models
+{
+    class Songs
+    {
+        public int SongId { get; set; }
+        public string SongName { get; set; }
+        public string SongFileCover { get; set; }
+        public string SongUrl { get; set; }
+        public string SongDuration { get; set; }
+        public string SingerName { get; set; }
+    }
+}
---
+++ MusicStreamingApplication/MusicStreamingApplication/MusicStreamingApplication/Services/SongsService.cs
@@ -0,0 +1,31 @@
+﻿using MusicStreamingApplication.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStreamingApplication.Services
+{
+    public class SongsService
+    {
+        private const string SongsApi = "https:7000/api/songs";
+        public async Task<List<Songs>> GetAllSongs(int pageNumber, int pageSize)
+        {
+            HttpClient httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync($"{SongsApi}?pageNumer={ pageNumber}&pageSize={ pageSize}");
+
+            return JsonConvert.DeserializeObject<List<Songs>>(json);
+        }
+
+
+        public async Task<List<Songs>> SearchSongs(string searchSongQuery)
+        {
+            HttpClient httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("https:7000/api/songs/SearchSongs?SearchSongQuery=" + searchSongQuery);
+
+            return JsonConvert.DeserializeObject<List<Songs>>(json);
+        }
+    }
+}
